0674a2e0b9d52b6770f58cec931bcbaee9dc8fff
DaOnlyOwner <jan@hemming-haas.de>
2018-05-06 14:07:41 +0200

    Implemented useful set of operators

---
+++ Hyra/src/Program.fs
@@ -0,0 +1,14 @@
+﻿module Main
+
+
+open SpecificCombinators
+
+[<EntryPoint>]
+let main argv = 
+    let x = satIdentifier
+    let input = Input.inputFromFile @"E:\F#Projects\Hyra\Tests\testprogram.hy"
+    printfn "Result: %A" (Combinators.run x input)
+    System.Console.ReadKey() |> ignore
+    0
+
+
